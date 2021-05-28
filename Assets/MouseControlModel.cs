e4ce796f2f53e1fc1231183a95fed1ca85b11c91
jerinezou <2421061825@qq.com>
2021-05-28 13:21:32 +0800

    代码

--- Assets/MouseControlModel.cs
+++ Assets/MouseControlModel.cs
@@ -4,65 +4,62 @@ using UnityEngine;
 
 public class MouseControlModel : MonoBehaviour
 {
-    //旋转最大角度
-    public int yMinLimit = 90;
-    public int yMaxLimit = 90;
-    //旋转速度
-    public float xSpeed = 250.0f;
-    public float ySpeed = 120.0f;
-    //旋转角度
-    private float x = 0.0f;
-    private float y = 0.0f;
+    Vector3 cubeScreenPos;
+    Vector3 offset;
+    void Start()
+    {
+        //隐藏或者显示物体
+        //transform.gameObject.SetActive(true);
+    }
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        //鼠标右键按下，旋转物体
+        if (Input.GetMouseButton(1))
         {
-            //将屏幕坐标转化为世界坐标  ScreenToWorldPoint函数的z轴不能为0，不然返回摄像机的位置，而Input.mousePosition的z轴为0
-            //z轴设成10的原因是摄像机坐标是（0，0，-10），而物体的坐标是（0，0，0），所以加上10，正好是转化后物体跟摄像机的距离
-            Vector3 temp = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
-            transform.position = temp;
+            float speed = 2.5f;//旋转跟随速度
+            float OffsetX = Input.GetAxis("Mouse X");//获取鼠标x轴的偏移量
+            float OffsetY = Input.GetAxis("Mouse Y");//获取鼠标y轴的偏移量
+            transform.Rotate(new Vector3(OffsetY, -OffsetX, 0) * speed, Space.World);//旋转物体
         }
-        else if (Input.GetMouseButton(1))
-        {
-            //Input.GetAxis("MouseX")获取鼠标移动的X轴的距离
-            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
-            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
-            y = ClampAngle(y, yMinLimit, yMaxLimit);
-            //欧拉角转化为四元数
-            Quaternion rotation = Quaternion.Euler(y, x, 0);
-            transform.rotation = rotation;
-        }
-        else if (Input.GetAxis("Mouse ScrollWheel") != 0)
-        {
-            //鼠标滚动滑轮 值就会变化
-            if (Input.GetAxis("Mouse ScrollWheel") < 0)
-            {
-                //范围值限定
-                if (Camera.main.fieldOfView <= 100)
-                    Camera.main.fieldOfView += 2;
-                if (Camera.main.orthographicSize <= 20)
-                    Camera.main.orthographicSize += 0.5F;
-            }
-            //Zoom in  
-            if (Input.GetAxis("Mouse ScrollWheel") > 0)
-            {
-                //范围值限定
-                if (Camera.main.fieldOfView > 2)
-                    Camera.main.fieldOfView -= 2;
-                if (Camera.main.orthographicSize >= 1)
-                    Camera.main.orthographicSize -= 0.5F;
-            }
-        }
-    }
 
-    //角度范围值限定
-    static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360)
-            angle += 360;
-        if (angle > 360)
-            angle -= 360;
-        return Mathf.Clamp(angle, min, max);
+        //鼠标滚轮滚动，放大缩小
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            //范围值限定
+            if (Camera.main.fieldOfView <= 100)
+                Camera.main.fieldOfView += 2;
+            if (Camera.main.orthographicSize <= 20)
+                Camera.main.orthographicSize += 0.5F;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            //范围值限定
+            if (Camera.main.fieldOfView > 2)
+                Camera.main.fieldOfView -= 2;
+            if (Camera.main.orthographicSize >= 1)
+                Camera.main.orthographicSize -= 0.5F;
+        }
+
+        cubeScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        //2. 计算偏移量
+        //鼠标的三维坐标
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cubeScreenPos.z);
+        //鼠标三维坐标转为世界坐标
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        offset = transform.position - mousePos;
+
+        //3. 物体随着鼠标移动
+        while (Input.GetMouseButton(0))
+        {
+            //目前的鼠标二维坐标转为三维坐标
+            Vector3 curMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cubeScreenPos.z);
+            //目前的鼠标三维坐标转为世界坐标
+            curMousePos = Camera.main.ScreenToWorldPoint(curMousePos);
+
+            //物体世界位置
+            transform.position = curMousePos + offset;
+        }
     }
 }
